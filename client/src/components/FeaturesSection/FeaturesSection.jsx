cf8b456158ee1b6cc30b163ccc0d85f2c127994d
JoshuaHM-p4 <joshuahm.2004@gmail.com>
2024-10-27 21:28:01 +0800

    refactor: Use JSON as dynamic data for FeaturesSection

--- client/src/components/FeaturesSection/FeaturesSection.jsx
+++ client/src/components/FeaturesSection/FeaturesSection.jsx
@@ -7,46 +7,46 @@ import featureIcon3 from '../../assets/svgs/today.svg';
 import featureIcon4 from '../../assets/svgs/clipboard.svg';
 
 const FeaturesSection = () => {
+
+  const featuresData = [
+    {
+      icon: featureIcon1,
+      title: "Stay Connected",
+      description: "Engage with fellow alumni through our dynamic message boards and forums. Share your experiences, ask questions, and stay in touch with friends and mentors from your alma mater."
+    },
+    {
+      icon: featureIcon2,
+      title: "Career Opportunities",
+      description: "Explore exclusive job listings and career opportunities. Our platform connects you with potential employers and offers resources to help you advance your career."
+    },
+    {
+      icon: featureIcon3,
+      title: "Events & Reunions",
+      description: "Never miss an event with our comprehensive event listings. Register for alumni reunions, webinars, and other networking opportunities directly through the portal."
+    },
+    {
+      icon: featureIcon4,
+      title: "Feedback & Surveys",
+      description: "Provide valuable feedback on your educational experience and participate in surveys. Help us improve our programs and services based on your insights and experiences"
+    }
+  ]
+
   return (
     <div className="features-section glass">
       <div className="container">
         <h2 className="section-title">FEATURES</h2>
         <div className="features-grid">
-          <div className="feature-card">
-            <img src={featureIcon1} alt="Stay Connected" className="feature-icon" />
-            <h3 className="feature-title">Stay Connected</h3>
-            <p className="feature-description">
-              Engage with fellow alumni through our dynamic message boards and forums.
-              Share your experiences, ask questions, and stay in touch with friends and mentors from your alma mater.
-            </p>
-          </div>
-
-          <div className="feature-card">
-            <img src={featureIcon2} alt="Career Opportunities" className="feature-icon" />
-            <h3 className="feature-title">Career Opportunities</h3>
-            <p className="feature-description">
-              Explore exclusive job listings and career opportunities. Our platform connects you
-              with potential employers and offers resources to help you advance your career.
-            </p>
-          </div>
-
-          <div className="feature-card">
-            <img src={featureIcon3} alt="Events & Reunions" className="feature-icon" />
-            <h3 className="feature-title">Events & Reunions</h3>
-            <p className="feature-description">
-              Never miss an event with our comprehensive event listings. Register for alumni reunions, webinars,
-              and other networking opportunities directly through the portal.
-            </p>
-          </div>
-
-          <div className="feature-card">
-            <img src={featureIcon4} alt="Feedback & Surveys" className="feature-icon" />
-            <h3 className="feature-title">Feedback & Surveys</h3>
-            <p className="feature-description">
-              Provide valuable feedback on your educational experience and participate in surveys.
-              Help us improve our programs and services based on your insights and experiences.
-            </p>
-          </div>
+          {
+            featuresData.map((feature, index) => {
+              return (
+                <div className='feature-card' key={index}>
+                  <img src={feature.icon} alt={feature.title} className='feature-icon' />
+                  <h3 className="feature-title">{feature.title}</h3>
+                  <p className="feature-description">{feature.description}</p>
+                </div>
+              )
+            })
+          }
         </div>
       </div>
     </div>
