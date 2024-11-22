db6e5516920d060a8ae8cca98473765140b39017
ymnwri <tiuprof77@gmail.com>
2024-11-22 16:30:42 +0800

    chore: enhance responsiveness UI on discussion page

--- client/src/components/DiscussionActionBar/DiscussionActionBar.jsx
+++ client/src/components/DiscussionActionBar/DiscussionActionBar.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import "./DiscussionActionBar.css";
 
 const DiscussionActionBar = ({ filter, setFilter, viewMode, setViewMode, onCreate }) => {
@@ -14,46 +13,47 @@ const DiscussionActionBar = ({ filter, setFilter, viewMode, setViewMode, onCreat
 
   return (
     <div className="discussion-actionbar mt-2">
-      <div className="d-flex gap-2">
-        {/* Filter Selection */}
-        <select
-          className="form-select py-0 discussion-selection"
-          onChange={handleFilterChange}
-          style={{ width: `${0.7 * filter.length + 4}rem` }}
-        >
-          <option selected value="best">
-            Best
-          </option>
-          <option value="relevancy">Relevancy</option>
-          <option value="latest">Latest</option>
-          <option value="mostReplies">Most Replies</option>
-          <option value="unanswered">Unanswered</option>
-          <option value="archived">Archived</option>
-        </select>
+      <div className="d-flex gap-2 action-bar-container">
+        {/* Dropdowns */}
+        <div className="discussion-selection-container">
+          <select
+            className="form-select py-0 discussion-selection"
+            onChange={handleFilterChange}
+            style={{ width: `${0.7 * filter.length + 4}rem` }}
+          >
+            <option selected value="best">
+              Best
+            </option>
+            <option value="relevancy">Relevancy</option>
+            <option value="latest">Latest</option>
+            <option value="mostReplies">Most Replies</option>
+            <option value="unanswered">Unanswered</option>
+            <option value="archived">Archived</option>
+          </select>
 
-        {/* Views Selection */}
-        <select
-          className="form-select py-0 discussion-selection"
-          onChange={handleViewChange}
-          style={{ width: `${0.5 * viewMode.length + 5}rem` }}
-        >
-          <option selected value="compact">
-            Compact
-          </option>
-          <option value="card">Card</option>
-        </select>
+          <select
+            className="form-select py-0 discussion-selection"
+            onChange={handleViewChange}
+            style={{ width: `${0.5 * viewMode.length + 5}rem` }}
+          >
+            <option selected value="compact">
+              Compact
+            </option>
+            <option value="card">Card</option>
+          </select>
+        </div>
 
-        {/* Search Bar */}
-        <input
-          type="text"
-          className="form-control py-0 discussion-search flex-grow-1"
-          placeholder="Search"
-        />
-
-        {/* Add New Thread Button */}
-        <button className="btn btn-primary py-0" onClick={onCreate}>
-          Create
-        </button>
+        {/* Search bar and "Create" button */}
+        <div className="search-and-button-container">
+          <input
+            type="text"
+            className="form-control py-0 discussion-search flex-grow-1"
+            placeholder="Search"
+          />
+          <button className="btn btn-primary py-0" onClick={onCreate}>
+            Create
+          </button>
+        </div>
       </div>
     </div>
   );
