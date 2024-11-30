e755ce93926aa3d257ece2f4340d6ab79ef90899
JoshuaHM-p4 <joshuahm.2004@gmail.com>
2024-12-01 00:29:11 +0800

    feat: add SearchBar component with search functionality and styling

---
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import searchIcon from "../../assets/svgs/search-outline.svg";
+import './SearchBar.css';
+
+const SearchBar = ({ onSearch, placeholder, buttonVisible }) => {
+  const [query, setQuery] = useState("");
+
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const handleSearch = () => {
+    if (onSearch) {
+      onSearch(query);
+    }
+  };
+
+  return (
+    <div className="search-bar-container input-group">
+      <input
+        type="text"
+        className="form-control py-0 discussion-search flex-grow-1"
+        placeholder={placeholder || 'Search'}
+        value={query}
+        onChange={(e) => {
+          handleInputChange(e);
+          if (!buttonVisible) {
+            handleSearch();
+          }
+        }}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            handleSearch();
+          }
+        }}
+      />
+      {buttonVisible && (
+        <button className="btn btn-outline-secondary">
+          <img className="search-icon" src={searchIcon} onClick={handleSearch} alt="Search Icon" />
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default SearchBar;
