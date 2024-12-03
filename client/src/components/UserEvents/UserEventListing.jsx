dca8971147ad1473671e3035194ba52ea1f9e25e
JoshuaHM-p4 <joshuahm.2004@gmail.com>
2024-12-03 23:55:40 +0800

    refactor: enhance styling and layout for UserEventListing and EventsFilterSection components

--- client/src/components/UserEvents/UserEventListing.jsx
+++ client/src/components/UserEvents/UserEventListing.jsx
@@ -3,31 +3,32 @@ import PropTypes from 'prop-types';
 import './UserEventListing.css';
 import fallbackImage from '../../assets/images/eventimage1.png';
 import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 const UserEventListing = ({ eventData }) => {
-  const { user }= useSelector(state => state.user);
+  const { user } = useSelector(state => state.user);
   const eventDate = new Date(eventData.event_date);
   const day = eventDate.getDate();
   const month = eventDate.toLocaleString('default', { month: 'short' });
   const formattedTitle = eventData?.event_name.replace(/\s+/g, '-');
 
-  // Truncate the description to a maximum of 200 characters
+  // Truncate the description to a maximum of 150 characters
   const truncatedDescription =
-    eventData?.description.length > 200
-      ? `${eventData.description.substring(0, 200)}...`
-      : eventData.description;
+    eventData?.description?.length > 150
+      ? `${eventData.description.substring(0, 150)}...`
+      : eventData.description || '';
 
   return (
     <div className="event-listing-auth">
       {/* Date Section */}
-      <div className="event-date-auth-section">
-        <div className="event-date-month-auth">{month}</div>
-        <div className="event-date-day-auth">{day}</div>
+      <div className="event-date-auth-section d-flex flex-column justify-content-center align-items-center gap-1 px-5">
+        <div className="event-date-month-auth p-0 m-0">{month}</div>
+        <div className="event-date-day-auth p-0 m-0">{day}</div>
       </div>
 
       {/* Event Details */}
-      <div className="event-image-and-details-auth">
+      <div className="d-flex event-details align-items-center flex-lg-row flex-column">
         {/* Event Image */}
         <div className="event-image-container-auth">
           <img
@@ -39,18 +40,19 @@ const UserEventListing = ({ eventData }) => {
         </div>
 
         {/* Event Information */}
-        <div className="event-details-auth">
-          <h5 className="event-title-auth">{eventData?.event_name}</h5>
+        <div className="event-information w-auto flex-grow-1 d-flex flex-column flex-grow-1">
+          <h5 className="event-title-auth" title={eventData?.event_name}>{eventData?.event_name}</h5>
           <p className="event-info-listing-auth">
-            <strong>{eventData?.location}</strong> | <span>{eventDate.toDateString()}</span>
+            <strong>{eventData?.location}</strong>
+            <span>{eventDate.toDateString()}</span>
           </p>
-          <p className="event-description-auth" dangerouslySetInnerHTML={{__html: truncatedDescription}}/>
-          <div className="event-additional-info-auth">
-            <span className="event-type-auth">{eventData.type}</span>
-            <Link to={`/events/${eventData?.event_id}`} className="btn more-info">
-              More Info
-            </Link>
-          </div>
+          <p className="event-description-auth d-flex flex-column w-100 wrap-text" dangerouslySetInnerHTML={{ __html: truncatedDescription }} />
+        </div>
+        <div className="event-info-action d-flex flex-column align-items-center h-100 justify-content-start">
+          <span className="event-type-text">{eventData.type}</span>
+          <Link to={`/events/${eventData?.event_id}`} className="btn more-info">
+            More Info
+          </Link>
         </div>
       </div>
     </div>
